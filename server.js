584056b2c27c9126ea2432fe2dd88f5120107489
Brrrandon-Lam <lamb@oregonstate.edu>
2020-05-31 16:15:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//from handlebars documentation page
 hbs = exphbs.create({
     // Specify helpers which are only registered on this instance.
     defaultLayout: 'main',
@@ -28,10 +29,12 @@ hbs = exphbs.create({
     extname: '.handlebars'
 });
 
+
+//Function renders the entire twit home page.
 app.get('/', function (req, res, next) {
     console.log("== Got a request")
     console.log("== Request Parameters:", req.params);
-    //shows all data for the page
+    //uses twitData.json to display information
     if (data) {
         var inputs = {
             twits: data,
@@ -43,23 +46,24 @@ app.get('/', function (req, res, next) {
         next();
     }
 });
-/*
+
+//Function will take in an index value and render only that twit for part 4 of the assignment (/twits/<INPUT>)
 app.get('/twits/:index', function (req, res, next) {
-    console.log("== Got a request")
+    console.log("== Got a request");
     console.log("== Request Parameters:", req.params);
-    var index = req.params.index;
+    var index = req.params.index; //stores req.params, which is the index of the twit we want to show
     if (index < data.length && index >= 0) {
-        var inputs = {
+        var input = {
             twits: [data[index]],
             show: 0
         }
-        res.render('twitPage', inputs);
+        res.render('twitPage', input);
     }
     else {
         res.status(404);
         res.render('404Page');
     }
-});*/
+});
 
 //Function that renders the 404 page on attempting to access something inaccessible.
 app.get('*', function (req, res, next) {
